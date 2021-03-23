69e5703ec7ba16fcda345adf110d65cddb1f442c
Zachary Stocks <stocks.zachary@gmail.com>
2021-03-23 00:41:23 -0600

    Created JavaScript object to hold quote data.

---
+++ js/quotes.js
@@ -0,0 +1,74 @@
+let quotes = [
+ {
+  quote: 'When day comes, we step out of the shade aflame and unafraid. The new dawn blooms as we free it. For there is always light. If only we’re brave enough to see it. If only we’re brave enough to be it.',
+  source: 'Amanda Gorman',
+  citation: 'Presidential Inaguration Poem',
+  year: 2021
+ },
+ {
+  quote: 'History isn\'t a single narrative, but thousands of alternative narratives. Whenever we choose to tell one, we are also choosing to silence others.',
+  source: 'Yuval Noah Harari',
+  citation: 'Homo Deus',
+  year: 2017
+ },
+ {
+  quote: 'Laugh and the world laughs with you; weep and you weep alone.',
+  source: 'Ella Wheeler Wilcox'
+ },
+ {
+  quote: 'The ancestor of every action is a thought.',
+  source: 'Ralph Waldo Emerson'
+ },
+ {
+  quote: 'This is the best reason to learn history: Not in order to predict the future, but to free yourself of the past and imagine alternative destinies.',
+  source: 'Yuval Noah Harari',
+  citation: 'Homo Deus',
+  year: 2017
+ },
+ {
+  quote: 'The mind is its own place, and in itself can make a Heav\'n of Hell, a Hell of Heav\'n.',
+  source: 'John Milton',
+  citation: 'Paradise Lost',
+  year: 1663
+ },
+ {
+  quote: 'We know of course, that there is no freedom of speech. But few persons realise that there is no freedom of silence either.',
+  source: 'Hu Shi',
+  citation: 'After being denounced as a counter-revolutionary by his son in Communist China'
+ },
+ {
+  quote: 'Oh, if there\'s one thing you learn at cookery school, burned crusts and soggy bases happen to the best of us. Roll up your sleeves and start something else I say. No point in moaning over what you can\'t fix!',
+  source: 'Mrs. Beamish',
+  citation: 'The Ickabog',
+  year: 2020
+ },
+ {
+  quote: 'Still, it was hard to have parents who lived inside you, when all you really wanted was for them to come back, and hug you.',
+  source: 'J.K. Rowling',
+  citation: 'The Ickabog',
+  year: 2020
+ },
+ {
+  quote: 'I do a better business when things are bad. I think the churches find the same thing is true.',
+  source: 'Zolar from "Dean of Astrologers"',
+  citation: 'The New Yorker',
+  year: 1959
+ },
+ {
+  quote: 'And though I have the gift of prophecy, and understand all mysteries, and all knowledge; and though I have all faith, so that I could remove mountains, and have not Love, I am nothing.',
+  source: 'Paul the Apostle',
+  citation: '1 Corinthians 13:2'
+ },
+ {
+  quote: 'You\'ve made just enough safe choices to stay alive, but not enough to matter. Is that what you want? You can be more; you want to be more, don\'t you? The window of opportunity is closing. This is your chance.',
+  source: 'Joe MacMillan',
+  citation: 'Halt and Catch Fire',
+  year: 2014
+ },
+ {
+  quote: 'She was the book thief without the words. Trust me, though, the words were on their way, and when they arrived, Liesel would hold them in her hands like the clouds, and she would wring them out like the rain.',
+  source: 'The Narrator',
+  citation: 'The Book Thief',
+  year: 2005
+ }
+]
