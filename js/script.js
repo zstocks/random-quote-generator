b7734cfcbb256dc27d0f354ff15548e910343a35
Zachary Stocks <stocks.zachary@gmail.com>
2021-03-24 14:39:48 -0600

    Random background colors and new properties.
    Added two new functions: getRandomColor() and randomizeBackgroundColor(). Together they build an RGB color code and use it as the background color for the body of the document every time a quote is printed.
    
    I also added two new optional properties to the quote objects: learnMore and link. They will be used to add a link to certain quotes that will allow the user to watch a trailer or buy a book that inspired the quote.

--- js/script.js
+++ js/script.js
@@ -26,7 +26,9 @@ let quotes = [
   quote: 'History isn\'t a single narrative, but thousands of alternative narratives. Whenever we choose to tell one, we are also choosing to silence others.',
   source: 'Yuval Noah Harari',
   citation: 'Homo Deus',
-  year: 2017
+  year: 2017,
+  learnMore: 'Buy the Book',
+  link: 'https://www.amazon.com/Homo-Deus-Brief-History-Tomorrow/dp/0062464345/ref=sr_1_1?dchild=1&keywords=homo+deus&qid=1616618129&sr=8-1'
  },
  {
   quote: 'Laugh and the world laughs with you; weep and you weep alone.',
@@ -40,7 +42,9 @@ let quotes = [
   quote: 'This is the best reason to learn history: Not in order to predict the future, but to free yourself of the past and imagine alternative destinies.',
   source: 'Yuval Noah Harari',
   citation: 'Homo Deus',
-  year: 2017
+  year: 2017,
+  learnMore: 'Buy the Book',
+  link: 'https://www.amazon.com/Homo-Deus-Brief-History-Tomorrow/dp/0062464345/ref=sr_1_1?dchild=1&keywords=homo+deus&qid=1616618129&sr=8-1'
  },
  {
   quote: 'The mind is its own place, and in itself can make a Heav\'n of Hell, a Hell of Heav\'n.',
@@ -57,13 +61,17 @@ let quotes = [
   quote: 'Oh, if there\'s one thing you learn at cookery school, burned crusts and soggy bases happen to the best of us. Roll up your sleeves and start something else I say. No point in moaning over what you can\'t fix!',
   source: 'Mrs. Beamish',
   citation: 'The Ickabog',
-  year: 2020
+  year: 2020,
+  learnMore: 'Buy the Book',
+  link: 'https://www.amazon.com/Ickabog-J-K-Rowling/dp/1338732870/ref=sr_1_1?dchild=1&keywords=the+ickabog&qid=1616618000&sr=8-1'
  },
  {
   quote: 'Still, it was hard to have parents who lived inside you, when all you really wanted was for them to come back, and hug you.',
-  source: 'J.K. Rowling',
+  source: 'The Narrator',
   citation: 'The Ickabog',
-  year: 2020
+  year: 2020,
+  learnMore: 'Buy the Book',
+  link: 'https://www.amazon.com/Ickabog-J-K-Rowling/dp/1338732870/ref=sr_1_1?dchild=1&keywords=the+ickabog&qid=1616618000&sr=8-1'
  },
  {
   quote: 'I do a better business when things are bad. I think the churches find the same thing is true.',
@@ -80,13 +88,17 @@ let quotes = [
   quote: 'You\'ve made just enough safe choices to stay alive, but not enough to matter. Is that what you want? You can be more; you want to be more, don\'t you? The window of opportunity is closing. This is your chance.',
   source: 'Joe MacMillan',
   citation: 'Halt and Catch Fire',
-  year: 2014
+  year: 2014,
+  learnMore: 'Watch the Trailer',
+  link: 'https://www.youtube.com/watch?v=pWrioRji60A'
  },
  {
   quote: 'She was the book thief without the words. Trust me, though, the words were on their way, and when they arrived, Liesel would hold them in her hands like the clouds, and she would wring them out like the rain.',
   source: 'The Narrator',
   citation: 'The Book Thief',
-  year: 2005
+  year: 2005,
+  learnMore: 'Buy the Book',
+  link: 'https://www.amazon.com/Book-Thief-Markus-Zusak/dp/0375842209/ref=sr_1_1?dchild=1&keywords=the+book+thief&qid=1616617836&sr=8-1'
  }
 ]
 
@@ -110,8 +122,16 @@ const getRandomQuote = function() {
 }
 
 /***
- * `randomizeBackgroundColor` function
+ * `getRandomColor` & `randomizeBackgroundColor` functions
 ***/
+const getRandomColor = function() {
+ return Math.floor(Math.random() * 251); //return a random number between 0 and 250
+}
+
+const randomizeBackgroundColor = function() {
+ let rgbColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`; //build an rgb color code
+ document.querySelector('body').style.backgroundColor = rgbColor; //set the background color of the body to the random rgb color code
+}
 
 /***
  * `printQuote` function
@@ -133,8 +153,9 @@ const printQuote = function() {
   pageContent += `<span class="year">${thisQuote.year}</span>`;
  }
  pageContent += `</p>`;
- document.getElementById('quote-box').innerHTML = pageContent; //Update the page with new quote content.
- clearInterval(timerId); //Reset the 30 second timer
+ randomizeBackgroundColor(); //set the background to a random color
+ document.getElementById('quote-box').innerHTML = pageContent; //Update the page with new quote content
+ clearInterval(timerId); //Reset the 20 second timer
  timerId = setInterval(printQuote, 20000);
 }
 
